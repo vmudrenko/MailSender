8012f2b522a0d986bedbcb14e5dbc1f3c50987a2
vmudrenko <vymudrenko@gmail.com>
2021-02-15 22:14:26 +0300

    Логика по отправке почты перенесена в приложение wpf

--- MailSender/MainWindow.xaml.cs
+++ MailSender/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Windows;
 
 namespace MailSender
 {
@@ -10,6 +13,30 @@ namespace MailSender
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void BtnSend_OnClick(object sender, RoutedEventArgs e)
+		{
+			var mailSender = new MailAddress(TbFrom.Text, TbFrom.Text);
+			var recipient = new MailAddress(TbTo.Text);
+
+			using var message = new MailMessage(mailSender, recipient)
+			{
+				Subject = TbSubject.Text,
+				Body = TbText.Text,
+			};
+
+			using (var client = new SmtpClient(TbServerAddress.Text, Convert.ToInt32(TbServerPort.Text)))
+			{
+				var login = TbLogin.Text;
+				var password = TbPassword.SecurePassword;
+
+				client.Credentials = new NetworkCredential(login, password);
+				client.EnableSsl = true;
+				client.Send(message);
+			}
+
+			MessageBox.Show(this, "Письмо отправлено", "Внимание");
 		}
 	}
 }
--- Tests/ConsoleTests/Program.cs
+++ Tests/ConsoleTests/Program.cs
@@ -8,36 +8,8 @@ namespace ConsoleTests
 	{
 		static void Main(string[] args)
 		{
-			var sender = new MailAddress("", "");
-			var recipient = new MailAddress("");
-
-			using var message = new MailMessage(sender, recipient)
-			{
-				Subject = "Тестовое сообщение",
-				Body = $"Текст письма: {DateTime.Now}",
-			};
-			
-			using (var client = new SmtpClient("smtp.mail.ru", 25))
-			{
-				var login = sender.Address;
-				var password = "";
-
-				client.Credentials = new NetworkCredential(login, password);
-				client.EnableSsl = true;
-				try
-				{
-					client.Send(message);
-				}
-				catch (Exception e)
-				{
-					Console.WriteLine(e);
-					throw;
-				}
-			}
-
 			Console.WriteLine("Почта отправлена");
 			Console.ReadLine();
-
 		}
 	}
 }
